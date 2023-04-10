9fc0d931e34c07890977ec6a47e5ececd11e64fa
GabeRundlett <gabe@rundlett.com>
2023-04-09 20:48:16 -0600

    [1.2.7] fix magicavoxel parse bug also fix the simple example in docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,55 +9,57 @@
 //! ```rust
 //! const BYTES: &[u8] = include_bytes!("palette.gvox");
 //! let mut o_buffer = Box::default();
-//!
 //! {
 //!     let gvox_ctx = gvox_rs::Context::new();
-//!
+//!     gvox_ctx.register_adapter::<gvox_rs::Parse, procedural_parse::Procedural>();
 //!     let o_config = gvox_rs::adapters::ByteBufferOutputAdapterConfig::from(&mut o_buffer);
-//!
 //!     let s_config = gvox_rs::adapters::ColoredTextSerializeAdapterConfig {
 //!         downscale_factor: 1,
 //!         downscale_mode: gvox_rs::adapters::ColoredTextSerializeAdapterDownscaleMode::Nearest,
 //!         non_color_max_value: 5,
+//!         vertical: false,
 //!     };
-//!
-//!     let mut i_ctx = gvox_ctx.get_adapter::<gvox_rs::Input, gvox_rs::adapters::ByteBuffer>()
-//!         .expect("Failed to get byte buffer input adapter.").create_adapter_context(BYTES)
+//!     let mut i_ctx = gvox_ctx
+//!         .get_adapter::<gvox_rs::Input, gvox_rs::adapters::ByteBuffer>()
+//!         .expect("Failed to get byte buffer input adapter.")
+//!         .create_adapter_context(PALETTE_BYTES)
 //!         .expect("Failed to create adapter context.");
-//!
-//!     let mut o_ctx = gvox_ctx.get_adapter::<gvox_rs::Output, gvox_rs::adapters::ByteBuffer>()
-//!         .expect("Failed to get byte buffer input adapter.").create_adapter_context(o_config)
+//!     let mut o_ctx = gvox_ctx
+//!         .get_adapter::<gvox_rs::Output, gvox_rs::adapters::ByteBuffer>()
+//!         .expect("Failed to get byte buffer output adapter.")
+//!         .create_adapter_context(o_config)
 //!         .expect("Failed to create adapter context.");
-//!     
-//!     let mut p_ctx = gvox_ctx.get_adapter::<gvox_rs::Parse, gvox_rs::adapters::GvoxPalette>()
-//!         .expect("Failed to get byte buffer input adapter.").create_adapter_context(())
+//!     let mut p_ctx = gvox_ctx
+//!         .get_adapter::<gvox_rs::Parse, gvox_rs::adapters::GvoxPalette>()
+//!         .expect("Failed to get gvox palette parse adapter.")
+//!         .create_adapter_context(())
 //!         .expect("Failed to create adapter context.");
-//!
-//!     let mut s_ctx = gvox_ctx.get_adapter::<gvox_rs::Serialize, gvox_rs::adapters::ColoredText>()
-//!         .expect("Failed to get byte buffer input adapter.").create_adapter_context(s_config)
+//!     let mut s_ctx = gvox_ctx
+//!         .get_adapter::<gvox_rs::Serialize, gvox_rs::adapters::ColoredText>()
+//!         .expect("Failed to get colored text serialize adapter.")
+//!         .create_adapter_context(s_config)
 //!         .expect("Failed to create adapter context.");
-//!
-//!     let region = gvox_rs::RegionRange {
-//!         offset: gvox_rs::Offset3D {
-//!             x: -4,
-//!             y: -4,
-//!             z: -4,
-//!         },
-//!         extent: gvox_rs::Extent3D { x: 8, y: 8, z: 8 },
-//!     };
-//!
 //!     gvox_rs::blit_region(
-//!         &mut i_ctx,
-//!         &mut o_ctx,
+//!         Some(&mut i_ctx),
+//!         Some(&mut o_ctx),
 //!         &mut p_ctx,
 //!         &mut s_ctx,
-//!         &region,
-//!         gvox_rs::ChannelId::COLOR | gvox_rs::ChannelId::NORMAL | gvox_rs::ChannelId::MATERIAL_ID,
-//!     ).expect("Error while translating.");
+//!         None, // Parse whole file!
+//!         gvox_rs::ChannelId::COLOR
+//!             | gvox_rs::ChannelId::NORMAL
+//!             | gvox_rs::ChannelId::MATERIAL_ID,
+//!     )
+//!     .expect("Error while translating.");
 //! }
-//!
-//! assert_eq!(22228, o_buffer.len(), "Buffer output length did not match expected.");
-//! println!("{}", std::str::from_utf8(&o_buffer).expect("Bad string slice."));
+//! assert_eq!(
+//!     33342,
+//!     o_buffer.len(),
+//!     "Buffer output length did not match expected."
+//! );
+//! println!(
+//!     "{}",
+//!     std::str::from_utf8(&o_buffer).expect("Bad string slice.")
+//! );
 //! ```
 //!
 //! ## Building
